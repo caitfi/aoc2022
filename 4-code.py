70b1347647f56013c203dba3b0364a20e5dd2935
Cait <75578333+caitfi@users.noreply.github.com>
2022-12-04 11:32:03 +0000

    Neater!

--- 4-code.py
+++ 4-code.py
@@ -2,9 +2,10 @@
 f = open("4-input.txt", "r")
 
 
-#### part 1 ####
+#### part 1 AND 2 ####
 
-total = 0
+subset = 0
+overlap = 0
 
 for line in f:
     # split into two ranges
@@ -17,29 +18,13 @@ for line in f:
 
     # compare if 0 is a subset of 1, or 1 is a subset of 0
     if (all (x in areaList[0] for x in areaList[1]) or all(x in areaList[1] for x in areaList[0])):
-        total += 1
-
-print(total)
-
-f.seek(0)
-
-#### part 2 ####
-
-total = 0
-
-for line in f:
-    # split into two ranges
-    assignments = line.split(",")
-    areaList = []
-
-    for ass in assignments:
-        boundaries = ass.split("-")
-        areaList.append(range(int(boundaries[0]), int(boundaries[1]) + 1))
+        subset += 1
     
     # intersection again?
     if set(areaList[0]).intersection(set(areaList[1])):
-        total += 1
+        overlap += 1
 
-print(total)
+print(subset)
+print(overlap)
 
 f.close()
